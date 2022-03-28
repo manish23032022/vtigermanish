d1c379fd00a5b74f2b35fba6b74aec4e7b72ab31
User <User@DESKTOP-PCQAT3M>
2022-03-28 16:22:11 +0530

    JenkinTest)2

---
+++ com.vtiger_test/src/test/java/com/vtiger_test_scripts/Demotest.java
@@ -0,0 +1,21 @@
+package com.vtiger_test_scripts;
+
+import org.testng.annotations.Test;
+
+public class Demotest {
+	@Test
+	public void demo1() {
+		
+		System.out.println("hello");
+		
+		
+		
+		System.out.println("helo");
+		
+		
+		
+		
+		System.out.println("ok");
+	}
+
+}
